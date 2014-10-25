b4168116ebd349d6409e580a1e856effb9ca094d
ryo33 <ryo12redstone@gmail.com>
2014-10-25 15:33:24 -0700

    can execute like readme

--- cuilib.py
+++ cuilib.py
@@ -2,7 +2,7 @@ import curses
 import curses.ascii
 import parse_command as pc
 
-class cuilib:
+class Cuilib:
     stdscr = None
     ENTER, LEFT, RIGHT, UP, DOWN, BS = 1, 2, 3, 4, 5, 6
     default_cursor = None
@@ -16,8 +16,11 @@ class cuilib:
         self.stdscr.keypad(True)
         curses.noecho()
 
-    def getargv(self):
+    def get_argv(self):
         return pc.parse_argv()
+
+    def parse_command(self, command):
+        return pc.parse(command)
 
     def init_typing(self):
         self.default_cursor = self.stdscr.getyx() #coordinate of first command char
@@ -38,38 +41,38 @@ class cuilib:
         while True:
             c = self.stdscr.getch()
             if curses.ascii.isprint(c):
-                self.type(chr(c))
+                self.__type(chr(c))
                 self.insert(chr(c))
                 if possibilities is not None:
                     possibilities = None
             elif c == curses.KEY_LEFT:
-                if self.left():
+                if self.__left():
                     possibilities = None
             elif c == curses.KEY_RIGHT:
-                if self.right():
+                if self.__right():
                     possibilities = None
             elif c == curses.KEY_ENTER:
                 self.newline
                 return pc.parse(command)
             elif c == curses.ascii.BS:
-                self.backspace()
+                self.__backspace()
             elif c == ord("\t") and func is not None:
                 if possibilities is None:
                     selected = 0
                     parsed = pc.parce(self.typing[0:self.cursor], True)
                     func(parsed)
                     if len(possibilities) > 0:
-                        self.print(possibilities[selected], self.default_cursor)
+                        self.__print(possibilities[selected], self.default_cursor)
                 elif len(possibilities) != 0:
                     selected = (selected + 1) % len(possibilities)
                     after = ""
                     if self.cursor < len(self.typing) - 1:
                         after = self.typing[self.cursor + 1:]
-                    self.print(possibilities[selected] + after, self.default_cursor)
+                    self.__print(possibilities[selected] + after, self.default_cursor)
 
-    def __getchar(self, func=None):
+    def __get_char(self, func=None):
         """
-        getcharacter
+        get_character
         func: example isprint, isalpha
         """
         while True:
@@ -78,9 +81,9 @@ class cuilib:
                 break
         return char
     
-    def getchar(self, func=None):
+    def get_char(self, func=None):
         """
-        getcharacter
+        get_character
         func: example isprint, isalpha
         """
         while True:
@@ -89,74 +92,91 @@ class cuilib:
                 break
         return chr(char)
 
-    def getstr(self, func=None):
+    def input(self, prompt, func=None):
+        self.__print(prompt)
+        return self.get_str(func)
+
+    def get_str(self, func=None):
         """
         getstring
         """
         self.init_typing()
         while True:
-            char = self.getchar()
-            if func is None or func(char):
-                type(char)
-                self.print(char)
-            elif char == curses.KEY_ENTER:
+            char = self.__get_char()
+            if char == 10 or char == curses.KEY_ENTER:
                 self.newline()
-                return string
+                return self.typing
+            elif func is None or func(char):
+                self.__type(char)
+                self.__print(chr(char))
 
-    def getpassword(self, func=curses.ascii.isgraph):
+    def get_password(self, func=curses.ascii.isgraph):
         """
         getpassword
         """
-        string = ""
+        self.init_typing()
         while True:
-            char = self.getchar()
-            if func(char):
-                string += char
-            elif char == curses.ascii.BS:
-
-            elif char == curses.KEY_ENTER:
+            char = self.__get_char()
+            if char == curses.KEY_ENTER or char == 10:
                 self.newline()
-                return string
+                return self.typing
+            elif func is None or func(char):
+                self.__type(char)
 
     def newline(self):
-        self.print("\n")
+        self.__print("\n")
 
-    def backspace(self):
-        if is_not_first_character():
-            self.print("\b")
+    def __backspace(self):
+        if __is_not_first_character():
+            self.__print("\b")
 
-    def type(self, char):
+    def __type(self, char):
         self.typing += chr(char)
         if self.cursor == self.cursor_max:
             self.cursor_max += 1
         self.cursor += 1
 
-    def is_not_first_character(self):
+    def __is_not_first_character(self):
         if self.cursor > 0:
             return True
         return False
 
-    def left(self):
-        if is_not_first_character():
+    def __left(self):
+        if __is_not_first_character():
             self.cursor -= 1
             self.move((self.default_cursor[0], self.default_cursor[1] + self.cursor))
             return True
         return False
-    def right(self):
+    
+    def __right(self):
         if self.cursor_max != self.cursor:
             self.cursor += 1
             self.move((self.default_cursor[0], self.default_cursor[1] + self.cursor))
             return True
         return False
 
-    def print(self, str, start=None):
+    def __print(self, text, start=None):
         if start is not None:
             move(start[0])
             self.stdscr.clrtoeol()
-        self.addstr(str)
+        self.stdscr.addstr(str(text))
+
+    def print(self, text, start=None):
+        if start is not None:
+            move(start[0])
+            self.stdscr.clrtoeol()
+        self.stdscr.addstr(str(text) + "\n")
 
     def insert(self, str):
         self.stdscr.insstr(str)
 
     def move(self, cursor):
         self.stdscr.move(cursor[0], cursor[1])
+
+
+def __wrapper(stdscr, main):
+    con = Cuilib(stdscr)
+    main(con)
+
+def wrapper(main):
+    curses.wrapper(__wrapper, main)
